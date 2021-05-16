7fb9a5f053a67472ef58a521aee21db429649cf6
nail449 <81387417+nail449@users.noreply.github.com>
2021-05-17 01:57:13 +0300

    Add files via upload

---
+++ kalkulyator1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace first_project
+{
+    class Kalkulyator
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("1-ci ededi daxil edin: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2-ci ededi daxil edin: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+           Console.Write("Hansi emeliyyati yerine yetirmek isteyirsiniz? ");
+            string emeliyyat = Console.ReadLine();
+
+            switch (emeliyyat)
+            {
+                case "+":
+                    Console.WriteLine("Netice: {0}", num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine("Netice: {0}", num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine("Netice: {0}",num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine("Netice: {0}",num1 / num2);
+                    break;
+                case "%":
+                    Console.WriteLine("Netice: {0}",num1 % num2);
+                    break;
+                default:
+                    Console.WriteLine("Teessuf ki,bele bir emeliyyat movcud deyil.");
+                    break;
+            }
+
+
+                }   
+    }
+}
